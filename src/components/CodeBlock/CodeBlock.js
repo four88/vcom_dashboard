59b4bc8411b7e8c132a36e239ff8462739558e12
Pharanyu <pharanyuchuenjit@gmail.com>
2022-11-29 01:18:02 +0000

    add documetnation route

--- src/components/CodeBlock/CodeBlock.js
+++ src/components/CodeBlock/CodeBlock.js
@@ -6,7 +6,7 @@ export default function CodeBlock({ code, lang }) {
 
   return (
 
-    <SyntaxHighlighter language={lang} style={tomorrowNightEightie} showLineNumbers="true">
+    <SyntaxHighlighter language={lang} style={tomorrowNightEighties} showLineNumbers="true">
       {code}
     </SyntaxHighlighter>
   )
